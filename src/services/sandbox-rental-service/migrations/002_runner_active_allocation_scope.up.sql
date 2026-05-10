074444b173566ebf6bac4f98ac5ca7872ab617da
Shovon Hasan <im.shovonhasan@gmail.com>
2026-05-10 02:22:30 -0700

    Consolidate sandbox rental migrations

--- src/services/sandbox-rental-service/migrations/002_runner_active_allocation_scope.up.sql
+++
@@ -1,6 +0,0 @@
-DROP INDEX IF EXISTS idx_runner_allocations_active_job;
-
-CREATE UNIQUE INDEX idx_runner_allocations_active_job
-    ON runner_allocations (provider, requested_for_provider_job_id)
-    WHERE requested_for_provider_job_id <> 0
-      AND state IN ('pending', 'jit_creating', 'jit_created', 'vm_submitted', 'runner_config_fetched');
